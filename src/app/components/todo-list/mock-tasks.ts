a6c4e056fdd4e8ebde2b1e52dff9289392d9475b
Kai-Jian Zhan (Derek) <derekjan1240@gmail.com>
2021-09-07 12:22:20 +0800

    TODO 假資料設置

---
+++ src/app/components/todo-list/mock-tasks.ts
@@ -0,0 +1,34 @@
+import { Task } from './dto/Task';
+
+export const FAKE_TASKS: Task[] = [
+  {
+    id: 1,
+    text: 'Hit the gym',
+    isChecked: false,
+  },
+  {
+    id: 2,
+    text: 'Pay bills',
+    isChecked: true,
+  },
+  {
+    id: 3,
+    text: 'Meet George',
+    isChecked: false,
+  },
+  {
+    id: 4,
+    text: 'Buy eggs',
+    isChecked: false,
+  },
+  {
+    id: 5,
+    text: 'Read a book',
+    isChecked: false,
+  },
+  {
+    id: 6,
+    text: 'Organize office',
+    isChecked: false,
+  },
+];
